319633cc0daec1acb5d4b3af1906e5b50d813b43
DELL <amine.m.a1998@gmail.com>
2021-02-13 13:15:44 +0100

    intro jsx and  rendering elements

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// part 1    introducing jsx
 
+/*
 function formatName(user) {
     return user.firstName + ' ' + user.lastName;
   }
   const user = {
     firstName: 'Kylian',
-    lastName: 'Mbappé'
+    lastName: 'Mbappé',
+    avatar : "https://www.clicavenue.com/wp-content/uploads/2020/04/arbre.jpg",
   };
-  const element = (<h1> HELLO, {formatName(user)} !</h1>);
+  //const element = (<h1> HELLO, {formatName(user)} !</h1>);
+    const  element= (<div>
+      <img src={user.avatar} alt="title"/>
+      <p>{user.firstName}</p>
+      <p>{user.lastName}</p>
+    </div>);
 
 ReactDOM.render(
     element
-,
+    ,
     document.getElementById('root')
 );
+ */
+
+//part 2 Rendering Elements
+function tick() {
+  const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+  );
+  ReactDOM.render(element, document.getElementById('roooot'));
+}
+setInterval(tick, 1000);
+

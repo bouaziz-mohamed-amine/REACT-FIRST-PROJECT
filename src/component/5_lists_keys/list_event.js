2290df8b91f38160cb98ea9a80317b70c3d59931
DELL <amine.m.a1998@gmail.com>
2021-02-20 11:54:25 +0100

    part 5  key

--- src/component/5_lists_keys/list_event.js
+++ src/component/5_lists_keys/list_event.js
@@ -4,23 +4,25 @@ class Key_List extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            books : [{title:'the prince',author:'najem',price:20},{title:'the king',author:'amine',price:50},{title:'learn react',author:'mark',price:1000,}]
+            books : [
+                {id : 1,  title:'the prince',author:'najem',price:20},
+                {id : 2,title:'the king',author:'amine',price:50},
+                {id : 3, title:'learn react',author:'mark',price:1000,}
+            ]
          }
     }
 
    rendrelist = (books) =>{
        return  books.map(book =>(
-       <li>
+       <li key={book.id} >
             <b>title : </b>  {book.title}
-                            <br/>    
-                            <b>author : </b>{book.title}
-                            <br/>
-                            <b>price : </b>{book.price }
-                            <hr></hr>            
+            <br/>    
+            <b>author : </b>{book.title}
+            <br/>
+            <b>price : </b>{book.price }
+            <hr></hr>            
        </li>))
    }
-
-
 
     render() {
         const title="Part 5";
@@ -31,16 +33,17 @@ class Key_List extends Component {
                <h1 style={{textAlign:"center"}}>{title} </h1>
                 
                 {
-                this.state.books.map(book =>
-                    <li>{book.title}</li>
+                this.state.books.map((book,index) =>
+                    <li key={index}>{book.title}</li>
                 )
                 }
+                
                 <br/>
                
                 {
                     this.state.books.map(book =>(
-                        <li>
-                            <b>title : </b>  {book.title}
+                        <li key={book.id}>
+                            <b >title : </b>  {book.title}
                             <br/>    
                             <b>author : </b>{book.title}
                             <br/>
